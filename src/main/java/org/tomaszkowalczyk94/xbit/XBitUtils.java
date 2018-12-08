0f56aacfc7763c6fad29a3648eb707fb86be33c7
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2018-12-08 20:28:36 +0100

    utils - increment for 16bits

--- src/main/java/org/tomaszkowalczyk94/xbit/XBitUtils.java
+++ src/main/java/org/tomaszkowalczyk94/xbit/XBitUtils.java
@@ -17,4 +17,11 @@ public class XBitUtils {
         return XBit8.valueOfUnsigned(unsignedValue);
     }
 
+    public static XBit16 incrementBy(XBit16 value, int incrementer) {
+        int unsignedValue = value.getUnsignedValue();
+        unsignedValue += incrementer;
+        unsignedValue = unsignedValue & XBit16.MAX_UNSIGNED_VALUE;
+        return XBit16.valueOfUnsigned(unsignedValue);
+    }
+
 }
--- src/test/java/org/tomaszkowalczyk94/xbit/XBitUtilsTest.java
+++ src/test/java/org/tomaszkowalczyk94/xbit/XBitUtilsTest.java
@@ -4,21 +4,39 @@ import junit.framework.TestCase;
 
 public class XBitUtilsTest extends TestCase {
 
-    public void testIncrementBy() {
+    public void testIncrementByFor8bit() {
         XBit8 value = XBit8.valueOfUnsigned(100);
         XBit8 incrementedValue = XBitUtils.incrementBy(value, 1);
         assertEquals(101, incrementedValue.getUnsignedValue());
     }
 
-    public void testIncrementByWithOverload() {
+    public void testIncrementByWithOverloadFor8bit() {
         XBit8 value = XBit8.valueOfUnsigned(255);
         XBit8 incrementedValue = XBitUtils.incrementBy(value, 1);
         assertEquals(0, incrementedValue.getUnsignedValue());
     }
 
-    public void testIncrementByNegativeValue() {
+    public void testIncrementByNegativeValueFor8bit() {
         XBit8 value = XBit8.valueOfUnsigned(0);
         XBit8 incrementedValue = XBitUtils.incrementBy(value, -1);
         assertEquals(255, incrementedValue.getUnsignedValue());
+    }
+
+    public void testIncrementByFor16bit() {
+        XBit16 value = XBit16.valueOfUnsigned(1000);
+        XBit16 incrementedValue = XBitUtils.incrementBy(value, 1);
+        assertEquals(1001, incrementedValue.getUnsignedValue());
+    }
+
+    public void testIncrementByWithOverloadFor16bit() {
+        XBit16 value = XBit16.valueOfUnsigned(65535);
+        XBit16 incrementedValue = XBitUtils.incrementBy(value, 1);
+        assertEquals(0, incrementedValue.getUnsignedValue());
+    }
+
+    public void testIncrementByNegativeValueFor16bit() {
+        XBit16 value = XBit16.valueOfUnsigned(0);
+        XBit16 incrementedValue = XBitUtils.incrementBy(value, -1);
+        assertEquals(65535, incrementedValue.getUnsignedValue());
     }
 }
