24092ab0bb40df3140f77d1ffd1cf906fb9726e5
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2018-12-28 01:56:28 +0100

    sub and add 8 bits

--- src/main/java/org/tomaszkowalczyk94/xbit/XBitUtils.java
+++ src/main/java/org/tomaszkowalczyk94/xbit/XBitUtils.java
@@ -1,9 +1,17 @@
 package org.tomaszkowalczyk94.xbit;
 
+import lombok.Builder;
+
 public class XBitUtils {
 
     private XBitUtils() {
         throw new IllegalStateException("Utility class");
+    }
+
+    public static class Arithmetic8bitResult {
+        public XBit8 result;
+        boolean carry = false;
+        boolean overflow = false;
     }
 
     /**
@@ -22,6 +30,36 @@ public class XBitUtils {
         unsignedValue += incrementer;
         unsignedValue = unsignedValue & XBit16.MAX_UNSIGNED_VALUE;
         return XBit16.valueOfUnsigned(unsignedValue);
+    }
+
+    public static XBit8 negativeOf(XBit8 value) {
+        int buf = (~value.getUnsignedValue()) & XBit8.MAX_UNSIGNED_VALUE;
+
+        return XBitUtils.incrementBy(
+                XBit8.valueOfUnsigned(buf),
+                1
+        );
+    }
+
+    public static Arithmetic8bitResult addTwo8bits(XBit8 value1, XBit8 value2) {
+        Arithmetic8bitResult ret = new Arithmetic8bitResult();
+        int result = value1.getUnsignedValue() + value2.getUnsignedValue();
+        if(result>XBit8.MAX_UNSIGNED_VALUE) {
+            ret.carry = true;
+        }
+        ret.result = XBit8.valueOfUnsigned(result & XBit8.MAX_UNSIGNED_VALUE);
+
+        ret.overflow =((value1.getBit(7) && value2.getBit(7) && !ret.result.getBit(7)) ||
+                (!value1.getBit(7) && !value2.getBit(7) && ret.result.getBit(7)));
+
+        return ret;
+    }
+
+    public static Arithmetic8bitResult subTwo8bits(XBit8 value1, XBit8 value2) {
+        return addTwo8bits(
+                value1,
+                negativeOf(value2)
+        );
     }
 
 }
--- src/test/java/org/tomaszkowalczyk94/xbit/XBitUtilsTest.java
+++ src/test/java/org/tomaszkowalczyk94/xbit/XBitUtilsTest.java
@@ -1,6 +1,7 @@
 package org.tomaszkowalczyk94.xbit;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
 
 public class XBitUtilsTest extends TestCase {
 
@@ -39,4 +40,81 @@ public class XBitUtilsTest extends TestCase {
         XBit16 incrementedValue = XBitUtils.incrementBy(value, -1);
         assertEquals(65535, incrementedValue.getUnsignedValue());
     }
+
+    public void testNegativeOf() {
+        assertEquals(-10,XBitUtils.negativeOf(
+                XBit8.valueOfSigned(10)
+        ).getSignedValue());
+
+        assertEquals(-127,XBitUtils.negativeOf(
+                XBit8.valueOfSigned(127)
+        ).getSignedValue());
+
+        assertEquals(127,XBitUtils.negativeOf(
+                XBit8.valueOfSigned(-127)
+        ).getSignedValue());
+
+        assertEquals(-128,XBitUtils.negativeOf(
+                XBit8.valueOfSigned(-128)
+        ).getSignedValue());
+    }
+
+    public void testAddTwo8bits() {
+        XBitUtils.Arithmetic8bitResult arithmetic8bitResult;
+
+        // ================
+        arithmetic8bitResult = XBitUtils.addTwo8bits(
+                XBit8.valueOfUnsigned(255),
+                XBit8.valueOfUnsigned(255)
+        );
+        Assert.assertEquals(254, arithmetic8bitResult.result.getUnsignedValue());
+        Assert.assertEquals(true, arithmetic8bitResult.carry);
+        Assert.assertEquals(false, arithmetic8bitResult.overflow);
+        // ================
+        arithmetic8bitResult = XBitUtils.addTwo8bits(
+                XBit8.valueOfSigned(127),
+                XBit8.valueOfSigned(127)
+        );
+        Assert.assertEquals(-2, arithmetic8bitResult.result.getSignedValue());
+        Assert.assertEquals(false, arithmetic8bitResult.carry);
+        Assert.assertEquals(true, arithmetic8bitResult.overflow);
+        // ================
+        arithmetic8bitResult = XBitUtils.addTwo8bits(
+                XBit8.valueOfUnsigned(0),
+                XBit8.valueOfUnsigned(255)
+        );
+        Assert.assertEquals(255, arithmetic8bitResult.result.getUnsignedValue());
+        Assert.assertEquals(false, arithmetic8bitResult.carry);
+        Assert.assertEquals(false, arithmetic8bitResult.overflow);
+        // ================
+        arithmetic8bitResult = XBitUtils.addTwo8bits(
+                XBit8.valueOfSigned(-20),
+                XBit8.valueOfSigned(-10)
+        );
+        Assert.assertEquals(-30, arithmetic8bitResult.result.getSignedValue());
+        Assert.assertEquals(true, arithmetic8bitResult.carry);
+        Assert.assertEquals(false, arithmetic8bitResult.overflow);
+        // ================
+        arithmetic8bitResult = XBitUtils.addTwo8bits(
+                XBit8.valueOfSigned(-128),
+                XBit8.valueOfSigned(-128)
+        );
+        Assert.assertEquals(0, arithmetic8bitResult.result.getSignedValue());
+        Assert.assertEquals(true, arithmetic8bitResult.carry);
+        Assert.assertEquals(true, arithmetic8bitResult.overflow);
+    }
+
+    public void testSubTwo8bits() {
+        XBitUtils.Arithmetic8bitResult arithmetic8bitResult;
+
+        // ================
+        arithmetic8bitResult = XBitUtils.subTwo8bits(
+                XBit8.valueOfSigned(50),
+                XBit8.valueOfSigned(10)
+        );
+        Assert.assertEquals(40, arithmetic8bitResult.result.getSignedValue());
+        Assert.assertEquals(true, arithmetic8bitResult.carry);
+        Assert.assertEquals(false, arithmetic8bitResult.overflow);
+    }
+
 }
